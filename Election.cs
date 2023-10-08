19a3bfa0396e6ec2de140636e5df30f8c79bbb2c
Aleksey1894 <oleksii.litvinchuk94@gmail.com>
2023-10-08 17:31:53 +0300

    Добавьте файлы проекта.

---
+++ Election.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace golosovanie
+{
+    internal class Election
+    {
+        private string _electionName;
+        public string ElectionName
+        {
+            get { return _electionName; }
+            set { _electionName = value; }
+        }
+        public Dictionary<string, int> Options { get; } = new Dictionary<string, int>();
+        public Dictionary<string, int> Votes { get; } = new Dictionary<string, int>();
+
+        public Election()
+        {
+            Options = new Dictionary<string, int>();
+            Votes = new Dictionary<string, int>();
+        }
+
+        public void AddOption(string option)
+        {
+            Options[option] = 0;
+        }
+
+        public void DisplayOptions()
+        {
+            Console.WriteLine("***Options***:");
+            foreach (var option in Options.Keys)
+            {
+                Console.WriteLine(option);
+            }
+        }
+
+        public void Vote(string option)
+        {
+            if (Options.ContainsKey(option))
+            {
+                Options[option]++;
+                Console.WriteLine($"Your vote for '{option}' has been added.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid option.");
+            }
+        }
+
+        public void DisplayResults()
+        {
+            Console.WriteLine($"Election Results for '{ElectionName}':");
+            foreach (var option in Options.Keys)
+            {
+                Console.WriteLine($"{option}: {Options[option]} votes");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using golosovanie;
+using System;
+using static System.Collections.Specialized.BitVector32;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Create a new election:");
+        Console.WriteLine("Enter the name of the election:");
+        string electionName = Console.ReadLine();
+
+        Election election = new Election();
+        election.ElectionName = electionName;
+
+        Console.WriteLine($"Election '{electionName}' has been created.");
+        Console.WriteLine("Enter options for the election (one option per line and string value). Enter 'done' to finish.");
+
+        while (true)
+        {
+            string option = Console.ReadLine();
+            if (option.ToLower() == "done")
+            {
+                break;
+            }
+
+            election.AddOption(option);
+        }
+
+        Console.WriteLine("Options have been set. Let voters cast their votes.");
+
+        while (true)
+        {
+            Console.WriteLine("Enter your vote (type 'exit' to see the results):");
+            string vote = Console.ReadLine();
+
+            if (vote.ToLower() == "exit")
+            {
+                break;
+            }
+
+            election.Vote(vote);
+        }
+
+        election.DisplayResults();
+    }
+}
